900232de8b437eeca4e39f9bac24fcdf03e6ebf7
michael-sorensen <86160279+michael-sorensen@users.noreply.github.com>
2021-10-19 18:00:43 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibWljaGFlbHNvcmVuc2VuIiwiYSI6ImNrdWQwM3RpeTE1czgzMW8zOGJhc2xyNnoifQ.wXxOVepL95FBN8Tpq1Cecg";
+var weather_key = "c1a99e7e750a4644d00484442200c303";
